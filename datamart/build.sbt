c67dcc264a2867e1ee983ac78b9cc982370b6503
timoniche <dmitriidulaev@gmail.com>
2024-05-17 14:31:56 +0300

    WebHdfs java Client (csv downloader api)

---
+++ datamart/build.sbt
@@ -0,0 +1,16 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.13.14"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "datamart"
+  )
+
+val sparkVersion = "3.5.1"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "org.apache.spark" %% "spark-mllib" % sparkVersion
+)
---
+++ datamart/src/main/java/hdfs/HdfsClient.java
@@ -0,0 +1,48 @@
+package hdfs;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
+public class HdfsClient {
+
+    private final String hdfsBaseUrl = "http://127.0.0.1:9870/webhdfs/v1";
+
+    public HdfsClient() {
+    }
+
+    public void download(
+            String hdfsFilePath,
+            String localDownloadPath
+    ) {
+        try {
+            URI uri = new URI(hdfsBaseUrl + hdfsFilePath + "?op=OPEN");
+            HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
+            connection.setRequestMethod("GET");
+
+            try (InputStream is = connection.getInputStream(); FileOutputStream fos = new FileOutputStream(localDownloadPath)) {
+                is.transferTo(fos);
+            }
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                System.out.println("File downloaded successfully from HDFS.");
+            } else {
+                System.out.println("Failed to download file from HDFS (HTTP Status: " + responseCode + ")");
+            }
+
+        } catch (IOException | URISyntaxException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        String hdfsFilePath = "/user/ddulaev/dataset.csv";
+        String rootPath = Paths.get(".").toAbsolutePath().normalize().toString();
+        String localDownloadPath = rootPath + "/src/dataset.csv";
+
+        new HdfsClient().download(hdfsFilePath, localDownloadPath);
+    }
+}
